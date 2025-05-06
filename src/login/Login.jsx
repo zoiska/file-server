194f7d8d3451c33507a86de4298a052b546fb567
zoiska <zoisk64@gmail.com>
2025-05-07 00:19:26 +0200

    readd sytlllle

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Login.css'
 
 function Login({ setIsAuthenticated}) {
     const navigate = useNavigate();
